e9d61819013454f861f84db0f2d4191884942443
TomPearson <tom_pearson@yahoo.com>
2020-06-02 11:47:51 -0700

    Feature 1 Maps added code and files. Not checked in and intended to never add to version controll is store.properties which contains keys

--- PockectKnifeModule1/src/main/java/com/example/pocketknifemodule1/MainActivity.kt
+++ PockectKnifeModule1/src/main/java/com/example/pocketknifemodule1/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.pocketknifemodule1
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -10,12 +12,14 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var toast: Toast
     private lateinit var textMessage: TextView
+    lateinit var button: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.button_Map_id)
 
         textMessage = findViewById(R.id.message)
         textMessage.setTextColor(Color.BLACK)
@@ -27,5 +31,14 @@ class MainActivity : AppCompatActivity() {
         textMessage.setText(getLocationStatus(this))
         textMessage.setText(getBlueToothStatus(this))
         textMessage.setText(getMACAddress(this))
+
+//        Map feature
+
+        button.setOnClickListener({ v ->
+            val intent: Intent = Intent(this, MapsActivity::class.java).apply {}
+            startActivity(intent)
+        })
+
+
     }
 }
