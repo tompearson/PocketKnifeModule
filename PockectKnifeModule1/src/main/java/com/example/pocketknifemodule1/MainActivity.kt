1bc0aeb10bfca7d16a1eaa5cc0d2495498f0cee4
Tom Pearson <tom_pearson@yahoo.com>
2020-06-11 11:57:55 -0700

    Configuring for continues deliver. Added signing including the Kotlin.jks keystore.

--- PockectKnifeModule1/src/main/java/com/example/pocketknifemodule1/MainActivity.kt
+++ PockectKnifeModule1/src/main/java/com/example/pocketknifemodule1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.pocketknifemodule1
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -11,6 +12,7 @@ import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes;
 import com.microsoft.appcenter.distribute.Distribute;
+import com.microsoft.appcenter.utils.async.AppCenterConsumer
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,6 +36,12 @@ class MainActivity : AppCompatActivity() {
             Analytics::class.java, Crashes::class.java, Distribute::class.java
         )
 
+        val future = Crashes.hasCrashedInLastSession()
+        future.thenAccept(AppCenterConsumer {
+            if(it){
+                Toast.makeText(this, "Oops! Sorry about that crash!", Toast.LENGTH_LONG).show()
+            }
+        })
         methodWithPermissions(this)
 
         textMessage.setText(getNetworkStatus(this))
@@ -43,10 +51,11 @@ class MainActivity : AppCompatActivity() {
 
 //        Map feature
 
-        button.setOnClickListener({ v ->
+        button.setOnClickListener {
+             Crashes.generateTestCrash()
             val intent: Intent = Intent(this, MapsActivity::class.java).apply {}
             startActivity(intent)
-        })
+        }
 
 
     }
